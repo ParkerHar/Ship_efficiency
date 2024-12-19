8cb107c1d214a7c448fabed2322a4a3d1761c53a
Parker <pharalds11@gmail.com>
2024-12-19 10:47:44 -0800

    functions created to begin exploring data

---
+++ fncts.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#create box plots
+def box_plots(df, col, title):
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(x=col, data=df)
+    plt.title(title)
+    plt.show()
+
+# create histograms
+def hist_plots(df, col, title):
+    plt.figure(figsize=(10, 6))
+    sns.histplot(df[col], bins=20, kde=True)
+    plt.title(title)
+    plt.show()
